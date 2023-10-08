52483ed84599bad6326ca70cfd6a991679470409
user <user@WIN-4HU5T7LM44J>
2023-10-08 20:02:27 +0300

    Добавьте файлы проекта.

---
+++ Homework25/Homework25.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+using namespace std;
+//void s()
+//{
+//    char line[100]{".,.,.,.,.,.,."};
+//
+//    for (int i = 0; i < 100; i++)
+//    {
+//        if (line[i] == '.') {
+//            line[i] = '!';
+//        }
+//    }
+//    cout << line;
+//}
+//int main()
+//{
+//    s();
+//}
+
+void s()
+{
+    char line[100]{ ".,.,.,.,.,.,." };
+    int l = 0;
+    for (int i = 0; i < 100; i++)
+    {
+        if (line[i] == '.') {
+            l++;
+        }
+    }
+    cout << l;
+}
+int main()
+{
+    s();
+}
